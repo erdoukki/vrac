7ce1f39f571d47061a2eaf5fa5fbef02261b5d65
Kerma Gérald <gandalf@gk2.net>
2021-09-27 09:21:07 +0200

    Update 50_extras.sh

--- openwrt/files/etc/uci-defaults/50_extras.sh
+++ openwrt/files/etc/uci-defaults/50_extras.sh
@@ -1,3 +1,4 @@
+## 202109270920
 ## Start of all extras
 cat << "EOAF" > /etc/uci-defaults/50_extras.sh
 ## opkg-extras
@@ -314,6 +315,57 @@ exit 1
 EOF
 cat << "EOF" >> /etc/sysupgrade.conf
 /etc/uci-defaults/90-extroot-restore
+EOF
+## extroot-init
+# Prepare extroot/overlay automatically
+mkdir -p /etc/hotplug.d/online
+cat << "EOF" > /etc/hotplug.d/online/49-extroot-init
+if [ ! -e /etc/extroot-init ] && lock -n /var/lock/extroot-init && opkg update then
+  . /etc/profile.d/opkg.sh
+  if ! uci -q get fstab.overlay > /dev/null then
+    DISK=/dev/mmcblk0
+    DEVICE=${DISK}p3
+    uci set opkg.rwm="opkg"
+    uci add_list opkg.rwm.ipkg="fdisk"
+    uci add_list opkg.rwm.ipkg="block-mount"
+    uci add_list opkg.rwm.ipkg="kmod-fs-f2fs"
+    uci add_list opkg.rwm.ipkg="f2fstools"
+    uci add_list opkg.rwm.ipkg="partx"
+    uci add_list opkg.rwm.ipkg="mount-utils"
+    uci commit opkg
+    opkg restore rwm
+    if [ ! -b ${DEVICE} ] then
+      # Add root fs before mount root
+      yes | fdisk -u ${DISK} << "EOCF"
+n
+p
+3
+1000000
+
+w
+EOCF
+      partx -d - ${DEVICE}
+      partx -a - ${DEVICE}
+    fi
+    if [ -b ${DEVICE} ] then
+      mkfs.f2fs -l rootfs_data  ${DEVICE}
+      eval $(block info ${DEVICE} | grep -o -e "UUID=\S*")
+      uci -q delete fstab.overlay
+      uci set fstab.overlay="mount"
+      uci set fstab.overlay.uuid="${UUID}"
+      uci set fstab.overlay.target="/overlay"
+      uci set fstab.overlay.enabled_fsck="1"
+      uci set fstab.overlay.enabled="1"
+      uci commit fstab
+      touch /etc/extroot-init
+      lock -u /var/lock/extroot-init
+      reboot
+    fi
+  fi
+fi
+EOF
+cat << "EOF" >> /etc/sysupgrade.conf
+/etc/hotplug.d/online/49-extroot-init
 EOF
 ## End of all extras
 EOAF
